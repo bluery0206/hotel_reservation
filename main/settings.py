a311c93a3e67095d20e331ecb9744813998ebc2f
aori___ <hilariomarkryan3@gmail.com>
2025-04-13 15:18:16 +0800

    Add emailing config

--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,10 +128,19 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = "/media/"
 
 
 # Unauthorized Accesses Redirect Path
-# LOGIN_REDIRECT_URL  = 'operate-index'
-# LOGIN_URL           = 'user-login'
+LOGIN_REDIRECT_URL  = 'app-index'
+
+
+# Send Email
+EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST          = 'smtp.gmail.com'
+EMAIL_USE_TLS       = True
+EMAIL_PORT          = 587
+EMAIL_HOST_USER     = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
