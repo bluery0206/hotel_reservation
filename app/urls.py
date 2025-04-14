fd048938a988e52fafc21d3f13d134401ef26d22
aori___ <hilariomarkryan3@gmail.com>
2025-04-14 09:51:02 +0800

    Refactor and extract signin and signup  as separate pages for easier backend handling

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
 	path("", views.index, name="app-index"),
+	path("signin/", views.signin, name="app-signin"),
+	path("signup/", views.signup, name="app-signup"),
 ]
