01e4657467e30ee4b7969e107ee8a4a00e885989
aori___ <hilariomarkryan3@gmail.com>
2025-04-15 12:29:10 +0800

    Install `python-dotenv` for custom environment variable handling

--- main/settings.py
+++ main/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv # for loading project env vars
+load_dotenv() # getting env vars
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -143,5 +146,5 @@ EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST          = 'smtp.gmail.com'
 EMAIL_USE_TLS       = True
 EMAIL_PORT          = 587
-EMAIL_HOST_USER     = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER     = os.environ("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ("EMAIL_HOST_PASSWORD")
