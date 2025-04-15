a8791f70a99a7a8ee1e0d5cd9e47e667aad136cb
aori___ <hilariomarkryan3@gmail.com>
2025-04-15 15:59:42 +0800

    Add forgot password logic so it is now functional

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
+from django.contrib.auth.views import (
+	PasswordResetView,
+	PasswordResetDoneView,
+    PasswordResetConfirmView,
+	PasswordResetCompleteView,
+)
 
 from . import views
 
@@ -7,4 +13,17 @@ urlpatterns = [
 	path("signin/", views.signin, name="app-signin"),
 	path("signup/", views.signup, name="app-signup"),
 	path("signout/", views.signout, name="app-signout"),
+
+	path("signin/forgot_password/reset/", PasswordResetView.as_view(
+		template_name='app/auth/password_reset/password_reset.html'
+        ),name="password-reset"),
+	path("signin/forgot_password/reset/confirm/<uidb64>/<token>/", PasswordResetConfirmView.as_view(
+        template_name='app/auth/password_reset/password_reset_confirm.html'),
+        name="password_reset_confirm"),
+	path("signin/forgot_password/reset/done/", PasswordResetDoneView.as_view(
+        template_name='app/auth/password_reset/password_reset_done.html'),
+        name="password_reset_done"),
+	path("signin/forgot_password/reset/complete/", PasswordResetCompleteView.as_view(
+        template_name='app/auth/password_reset/password_reset_complete.html'),
+        name="password_reset_complete"),
 ]
