f2980c48f86030ddcf7ab6fee0a000ce15b62024
aori___ <hilariomarkryan3@gmail.com>
2025-04-17 11:53:09 +0800

    Add `Booking` to bookings, `Amenity` for extra services provided in a room, `Room`, and `RoomImage` to store different images as FileField doesnt support multiple files

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 
-from .models import Profile
+from .models import (
+    Profile,
+    Booking,
+    Amenity,
+    Room,
+    RoomImage
+)
 
-admin.site.register(Profile)
+admin.site.register([
+    Profile,
+    Booking,
+    Amenity,
+    Room,
+    RoomImage
+])
--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django_resized import ResizedImageField
 
+
 class Profile(models.Model):
     """ Profile """
     user = models.OneToOneField(
@@ -17,3 +18,46 @@ class Profile(models.Model):
         null = True,
         default = "default.png",
     )
+
+
+class Amenity(models.Model):
+    """ Amenities """
+    name = models.CharField(max_length=100)
+
+
+class Room(models.Model):
+    """ Room """
+
+    class RoomTypes(models.IntegerChoices):
+        """ Room Types"""
+
+        STANDARD = 0, "Standard"
+        DELUXE = 1, "Deluxe"
+        SUITE = 2, "Suit"
+
+    type = models.IntegerField(choices=RoomTypes.choices)
+    amenities = models.ManyToManyField(Amenity)
+    base_price = models.DecimalField(max_digits=10, decimal_places=2)
+    capacity = models.IntegerField()
+
+
+class RoomImage(models.Model):
+    """ Room Images """
+    room = models.OneToOneField(Room, on_delete=models.CASCADE, related_name='room')
+    image = ResizedImageField(
+        upload_to = 'profile_images',
+        blank = True,
+        null = True,
+        default = "default.png",
+    )
+
+class Booking(models.Model):
+    """ Booking """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    check_in = models.DateTimeField()  # Start date
+    check_out = models.DateTimeField()  # End date
+    booked_at = models.DateTimeField(auto_now_add=True) 
+    book_until = models.DateTimeField()
+    paid_price = models.DecimalField(max_digits=10, decimal_places=2)  # Final price after discounts
+    discount = models.DecimalField(max_digits=5, decimal_places=2, default=0)  # e.g., 10.50%
